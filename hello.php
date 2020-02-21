bb3175bf2d4966748d57cc4f68f2b3af6e645db0
wanda <wanda-8106@outlook.jp>
2020-02-21 23:39:55 +0900

    再提出

---
+++ hello.php
@@ -0,0 +1,33 @@
+<?php
+
+//課題1
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+//課題2
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+print $array_month[7];
+
+//課題3
+$hello = 'Hello';
+$name = 'yanagisawa';
+$world = "'s World!";
+$hello .=$name;
+$hello .=$world;
+echo $hello;
+
+//課題4
+
+$tech_boost = 'tech';
+$tech_boost .= ' boost';
+echo $tech_boost;
+
+//課題5
+echo '変数は数字で始まる名前は付けられない。';
+echo '変数はarray()か[]で囲む';
+echo 'Feburaryを定義しているとき、で区切っている';
+echo 'Octoberの値が文字列なのに\'\'で囲まれていない';
+echo 'echoで表示するとき、[December]ではなく[\'December\']にする。';
+
+?>
